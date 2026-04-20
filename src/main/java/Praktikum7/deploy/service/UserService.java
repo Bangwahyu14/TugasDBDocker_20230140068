f784c1c058dbb3ea4eca4fd96177da8187480e44
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2026-04-20 10:24:54 +0700

    menambahkan publick userservice ,adduser, listuser

---
+++ src/main/java/Praktikum7/deploy/service/UserService.java
@@ -0,0 +1,30 @@
+package Praktikum7.deploy.service;
+
+
+import Praktikum7.deploy.model.User;
+import Praktikum7.deploy.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User addUser(User request) {
+        request.setId(UUID.randomUUID().toString());
+        return userRepository.save(request);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+
+}

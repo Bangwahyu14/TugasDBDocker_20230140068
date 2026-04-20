462eb96e55907ced875ed8e9dfcde7573962dece
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2026-04-20 10:26:30 +0700

    mengubah user model

--- src/main/java/Praktikum7/deploy/repository/UserRepository.java
+++ src/main/java/Praktikum7/deploy/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package Praktikum7.deploy.repository;
 
 
-import org.apache.catalina.User;
+
+import Praktikum7.deploy.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User,String> {
+public interface UserRepository extends JpaRepository<User, String> {
 }
